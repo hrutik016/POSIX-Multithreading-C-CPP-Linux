350ea9e9ae4ad22920e459e5780c283fd8e2a18b
hrutik016 <hrutik716@gmail.com>
2024-07-09 00:29:01 -0400

    soution.exe added and running

--- Part A - Multithreading/#8 Consumer Producer/compile.sh
+++ Part A - Multithreading/#8 Consumer Producer/compile.sh
@@ -1,5 +1,4 @@
 gcc -g -c Queue.c -o Queue.o
-gcc -g -c Assignment_prod_cons_on_Q.c -o Assignment_prod_cons_on_Q.o
-gcc -g -c Assignment_prod_cons_on_Q_Solution.c -o Assignment_prod_cons_on_Q_Solution.o
-gcc -g Assignment_prod_cons_on_Q.o Queue.o -o exe -lpthread
-gcc -g Assignment_prod_cons_on_Q_Solution.o Queue.o -o solution.exe -lpthread
+gcc -g -c prod_cons_on_Q.c -o _prod_cons_on_Q.o
+gcc -g prod_cons_on_Q.o Queue.o -o exe -lpthread
+gcc -g prod_cons_on_Q.o Queue.o -o solution.exe -lpthread
